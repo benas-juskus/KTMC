e3b4ff082b41a9c21cb4fbf5684e7628bf0b0f04
benas-juskus <benas.juskus@stud.ktmc.lt>
2023-11-21 22:36:21 +0200

    Homework update

--- E Alminiene/DB/DB Prekyba 2023-11-15/uzklausos.sql
+++ E Alminiene/DB/DB Prekyba 2023-11-15/uzklausos.sql
@@ -11,7 +11,21 @@ ORDER BY atlygis DESC
 LIMIT 1;
 
 -- 3.
-SELECT makl.makl_nr, makl.vardas, makl.pavarde, pard.pirkejas, pirk.vardas, pirk.pavarde
+SELECT makl.vardas, makl.pavarde, COUNT(pirkejas) AS pirkeju_skaicius
 FROM pard
-JOIN makl ON makl.makl_nr = pard.makleris
-JOIN pirk ON pirk.pirk_nr = pard.pirkejas;
+JOIN makl ON pard.makleris = makl.makl_nr
+GROUP BY makleris;
+
+-- 4.
+SELECT pard_data AS pardavimo_data, SUM(apmoketa) AS apyvarta
+FROM pard
+GROUP BY pard_data
+ORDER BY apyvarta DESC
+LIMIT 1;
+
+-- 5.
+SELECT vardas, pavarde
+FROM pirk
+WHERE valstija = 'CA';
+
+-- 6. ?????????
